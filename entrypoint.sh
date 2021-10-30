6e51e65de1b531efdced2b5913348345a7cde0d5
Khorram Khondkar <khorramk.kbsk@gmail.com>
2021-10-30 14:41:34 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,24 +6,24 @@ SSH_PORT=$3
 PATH_SOURCE=$4
 OWNER=$5
 
-mkdir -p /root/.ssh
-ssh-keyscan -H "$SSH_HOST" >> /root/.ssh/known_hosts
+mkdir -p .ssh
+ssh-keyscan -H "$SSH_HOST" >> .ssh/known_hosts
 
 if [ -z "$DEPLOY_KEY" ];
 then
 	echo $'\n' "------ DEPLOY KEY NOT SET YET! ----------------" $'\n'
 	exit 1
 else
-	printf '%b\n' "$DEPLOY_KEY" > ~/.ssh/id_rsa
-	chmod 400 ~/.ssh/id_rsa
+	printf '%b\n' "$DEPLOY_KEY" > .ssh/id_rsa
+	chmod 400 .ssh/id_rsa
 
 	echo $'\n' "------ CONFIG SUCCESSFUL! ---------------------" $'\n'
 fi
 
 if [ ! -z "$SSH_PORT" ];
 then
-        printf "Host %b\n\tPort %b\n" "$SSH_HOST" "$SSH_PORT" > ~/.ssh/config
-	ssh-keyscan -p $SSH_PORT -H "$SSH_HOST" >> ~/.ssh/known_hosts
+        printf "Host %b\n\tPort %b\n" "$SSH_HOST" "$SSH_PORT" > .ssh/config
+	ssh-keyscan -p $SSH_PORT -H "$SSH_HOST" >> .ssh/known_hosts
 fi
 
 rsync --progress -avzh \
@@ -43,10 +43,10 @@ then
 	echo $'\n' "------ SYNC SUCCESSFUL! -----------------------" $'\n'
 	echo $'\n' "------ RELOADING PERMISSION -------------------" $'\n'
 
-	ssh -i ~/.ssh/id_rsa -t $SSH_USER@$SSH_HOST "sudo chown -R $OWNER:$OWNER $PATH_SOURCE"
-	ssh -i ~/.ssh/id_rsa -t $SSH_USER@$SSH_HOST "sudo chmod 775 -R $PATH_SOURCE"
-	ssh -i ~/.ssh/id_rsa -t $SSH_USER@$SSH_HOST "sudo chmod 777 -R $PATH_SOURCE/storage"
-	ssh -i ~/.ssh/id_rsa -t $SSH_USER@$SSH_HOST "sudo chmod 777 -R $PATH_SOURCE/public"
+	ssh -i .ssh/id_rsa -t $SSH_USER@$SSH_HOST "sudo chown -R $OWNER:$OWNER $PATH_SOURCE"
+	ssh -i .ssh/id_rsa -t $SSH_USER@$SSH_HOST "sudo chmod 775 -R $PATH_SOURCE"
+	ssh -i .ssh/id_rsa -t $SSH_USER@$SSH_HOST "sudo chmod 777 -R $PATH_SOURCE/storage"
+	ssh -i .ssh/id_rsa -t $SSH_USER@$SSH_HOST "sudo chmod 777 -R $PATH_SOURCE/public"
 
 	echo $'\n' "------ CONGRATS! DEPLOY SUCCESSFUL!!! ---------" $'\n'
 	exit 0
